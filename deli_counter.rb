b4778a8fdc5290d2a485f7d337f35473706d1b15
Makeda P Mckune <makedamckune@gmail.com>
2019-10-11 08:14:05 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -4,3 +4,9 @@ counter = 0
 puts "The line is currently empty."
 counter += 0
 end 
+
+def take_a_number
+  currently_on_line = ["Katie", "Brock", "Stacey", "Brad"]
+  currently_on_line << "Becky", "Dawn", "Lisa"
+  counter = 1 
+  
